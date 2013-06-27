01ab4d53c650133b3316618e2eac3a0a6d9415bc
Gloton <gloton@gmail.com>
2013-06-27 17:33:41 -0400

    Obtencion de datos de BD ok.

--- mail-personalizado/index.php
+++ mail-personalizado/index.php
@@ -1,6 +1,7 @@
 <?php 
 include_once '../../lib/class.php';
-include_once '../../lib/class.php';
+$sql_productos_mail = "SELECT * FROM mails_personalizados;";
+$query_productos_mail = mysql_query($sql_productos_mail, Conectar::con()) or die("No se realizo la consulta");
 ?>
 <!DOCTYPE HTML>
 <html lang="es">
@@ -19,7 +20,15 @@ include_once '../../lib/class.php';
 		}
 		.table-striped tbody > tr:nth-child(2n+1) > td, 
 		.table-striped tbody > tr:nth-child(2n+1) > th {
+			background-color: #72A603;
+		}
+		.table-hover tbody tr:hover > td, .table-hover tbody tr:hover > th {
 			background-color: #C9F235;
+			color: #FFFFFF;
+		
+		}
+		.table-striped tbody > tr:nth-child(2n+1) > td, .table-striped tbody > tr:nth-child(2n+1) > th {
+			color: #FFFFFF;
 		}
 	</style>	
 </head>
@@ -27,34 +36,33 @@ include_once '../../lib/class.php';
 	<div class="container">
 		<div class="row">
 			<div class="span12">
-				<table class="table table-striped table-bordered">
+				<table class="table table-striped table-bordered table-hover">
         			<thead>
 						<tr>
-						<th>#</th>
-						<th>Nombre</th>
-						<th>Apellido</th>
-						<th>Usuario</th>
+							<th>#</th>
+							<th>Nombre remitente</th>
+							<th>Mail remitente</th>
+							<th>Nombre destinatario</th>
+							<th>Mail destinatario</th>
+							<th>&nbsp;</th>
 						</tr>
 					</thead>
 					<tbody>
+					<?php while ($fila=mysql_fetch_array($query_productos_mail)) : ?>
 						<tr>
-						<td>1</td>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
+							<td><?php echo ++$i; ?></td>
+							<td><?php echo $fila["nombre_remitente"]; ?></td>
+							<td><?php echo $fila["mail_remitente"]; ?></td>
+							<td><?php echo $fila["nombre_destinatario"]; ?></td>
+							<td><?php echo $fila["mail_destinatario"]; ?></td>
+							<td style="text-align: center;">
+								<a class="btn" href="">
+									<i class="icon-comment"></i>
+									Ver
+								</a>
+							</td>
 						</tr>
-						<tr>
-							<td>2</td>
-							<td>Jacob</td>
-							<td>Thornton</td>
-							<td>@fat</td>
-						</tr>
-						<tr>
-							<td>3</td>
-							<td>Larry</td>
-							<td>the Bird</td>
-							<td>@twitter</td>
-						</tr>
+					<?php endwhile; ?>
 					</tbody>
 				</table>			
 			</div>
